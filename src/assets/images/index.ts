294a4cd597ee01a4cf3673f8ddd08db391cc1a55
Gautam Mishra <njoygautam12@gmail.com>
2025-02-19 01:15:14 +0530

    Fix asset import case sensitivity for college image

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,6 +1,6 @@
 import hero from './hero.jpg'
 import zs from './zs.png'
-import college from './college.png'
+import college from './College.png'
 import logo from './logo.svg'
 import instinger from './instinger.png'
 import cf from './cf.png'
